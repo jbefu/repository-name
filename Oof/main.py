b61f15c2b5f781292058c910ea70b89160252cab
josephbefu <43292177+josephbefu@users.noreply.github.com>
2018-09-16 09:20:35 +0100

    the oof
    oof

---
+++ Oof/main.py
@@ -0,0 +1,95 @@
+import os
+
+print("")
+print("Welcome!")
+User = input("User:  ")
+Pass = input("Pass:  ")
+if User == ("Joseph"):
+    mas = 1
+if Pass == ("OOF"):
+    ter = 1
+if mas and ter == 1:
+    print("Welcome " + User)
+else:
+    print("Incorrect!")
+    exit()
+
+print("Music:")
+print("1.Soviet Russian Anthom")
+print("2.Rick Rolled")
+print("3.Rustie-Attak")
+print("4.Mario Slide")
+print("5.Thomas The Tank (Earrape)")
+print("6.Watch Dogs 2 Menu")
+song = input("Input the number of the song you want to play:  ")
+
+if song == ("1"):
+    os.startfile("1.mp3")
+if song == ("2"):
+    os.startfile("2.mp3")
+if song == ("3"):
+    os.startfile("3.mp3")
+if song == ("4"):
+    os.startfile("4.mp3")
+if song == ("5"):
+    os.startfile("5.mp3")
+if song == ("6"):
+    os.startfile("6.mp3")
+
+song = input("Input the number of the song you want to play:  ")
+
+if song == ("1"):
+    os.startfile("1.mp3")
+if song == ("2"):
+    os.startfile("2.mp3")
+if song == ("3"):
+    os.startfile("3.mp3")
+if song == ("4"):
+    os.startfile("4.mp3")
+if song == ("5"):
+    os.startfile("5.mp3")
+if song == ("6"):
+    os.startfile("6.mp3")
+
+song = input("Input the number of the song you want to play:  ")
+
+if song == ("1"):
+    os.startfile("1.mp3")
+if song == ("2"):
+    os.startfile("2.mp3")
+if song == ("3"):
+    os.startfile("3.mp3")
+if song == ("4"):
+    os.startfile("4.mp3")
+if song == ("5"):
+    os.startfile("5.mp3")
+if song == ("6"):
+    os.startfile("6.mp3")
+
+song = input("Input the number of the song you want to play:  ")
+
+if song == ("1"):
+    os.startfile("1.mp3")
+if song == ("2"):
+    os.startfile("2.mp3")
+if song == ("3"):
+    os.startfile("3.mp3")
+if song == ("4"):
+    os.startfile("4.mp3")
+if song == ("5"):
+    os.startfile("5.mp3")
+if song == ("6"):
+    os.startfile("6.mp3")
+
+print("Goodbye comrad! The Soviet Union will miss you!")
+os.startfile("1.mp3")
+exit()
+
+
+
+
+
+
+    
+
+
